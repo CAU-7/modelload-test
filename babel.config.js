03081f286b5a240a50d7b010ef5c754c456f6bf4
yuyunjae <yuyunjae@naver.com>
2024-11-29 16:47:11 +0900

    reset.. go camera

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,4 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    'react-native-reanimated/plugin', // 반드시 plugins 배열의 마지막에 위치해야 함
-  ],
+  plugins: ['react-native-reanimated/plugin'],
 };
